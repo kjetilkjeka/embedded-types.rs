a97fef3cac4755a145bfe1d44b4734139e4437e3
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-10-05 15:33:25 +0200

    added functions to turn non-blocking transmit/receive into the blocking variant

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,3 +1,24 @@
+/// `blocking_transmit` turns a non-blocking transmit into a blocking transmit
+pub fn blocking_transmit<F, O>(transmit: F) -> Result<O, TransmitError>
+    where F: Fn() -> Result<O, TransmitError> {
+    loop {
+        match transmit() {
+            Err(TransmitError::BufferFull) => (),
+            x => return x,
+        }            
+    }
+}
+
+/// `blocking_receive` turns a non-blocking receive into a blocking receive
+pub fn blocking_receive<F, O>(receive: F) -> Result<O, ReceiveError>
+    where F: Fn() -> Result<O, ReceiveError> {
+    loop {
+        match receive() {
+            Err(ReceiveError::BufferEmpty) => (),
+            x => return x,
+        }            
+    }
+}
 
 /// Common transmit errors.
 /// This list is intended to grow over time and it is not recommended to exhaustively match against it.
