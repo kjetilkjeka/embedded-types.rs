37bcdd5578542bbf8a02e7f89a5d9907dc538741
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-10-05 15:32:06 +0200

    Added ReceiveError similarly to TransmitError

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -4,3 +4,9 @@
 pub enum TransmitError {
     BufferFull,
 }
+
+/// Common receive errors.
+/// This list is intended to grow over time and it is not recommended to exhaustively match against it.
+pub enum ReceiveError {
+    BufferEmpty,
+}
